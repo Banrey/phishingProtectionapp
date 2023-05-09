aa0c8d0e7be65eb2bff1aaca81901347a60156ba
Banrey <114892207+Banrey@users.noreply.github.com>
2023-05-09 15:03:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/phishingprotectionapp/VideoGallery.java
@@ -0,0 +1,13 @@
+package com.example.phishingprotectionapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoGallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_gallery);
+    }
+}
